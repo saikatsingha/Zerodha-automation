8f35d88a37c295cfc80056a5d39e2aeb75f0f537
saikatsingha <saikatsingha@gmail.com>
2022-04-11 01:05:09 +0530

    Zerodha Simple sutomation  completed. Testing needed

--- automation_by_saikat.py
+++ automation_by_saikat.py
@@ -11,4 +11,18 @@ access_token = input("Access Token :")
 
 kite = KiteConnect(api_key)
 kite.set_access_token(access_token)
-print(kite)
+
+stock = input("Name of Stock :")
+Qty = input("Quantity :")
+transaction_type = input("Type BUY/SELL to buy or sell :")
+price = input("Price :")
+if (transaction_type == 'BUY') :
+    trigger_price = float (price) * 0.99
+else :
+    trigger_price = float (price) * 1.01
+# data = kite.generate_session(access_token,api_key)
+# kite.set_access_token(data["access_token"])
+# print(data)
+
+kite.place_order(tradingsymbol=stock, variety='regular', exchange='NSE', transaction_type=transaction_type,
+                     quantity=Qty, order_type='MARKET', product='CNC', price=price, trigger_price=trigger_price)
